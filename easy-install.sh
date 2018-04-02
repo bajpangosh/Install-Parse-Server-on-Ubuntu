021d148eb245e82532d69e4c5fe419a38a6a46c8
bajpangosh <bajpangosh@users.noreply.github.com>
2018-04-02 17:24:51 +0530

    Add files via upload

---
+++ easy-install.sh
@@ -0,0 +1,45 @@
+echo 'Wellcome to Parse Server and Dashboard on Ubuntu install script';
+sleep 2;
+cd ~
+echo 'installing python-software-properties';
+sleep 2;
+apt-get install -y build-essential git python-software-properties
+
+echo 'installing Node Js';
+sleep 2;
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+apt-get install -y build-essential
+
+echo 'installing Mongo DB';
+sleep 2;
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org
+service mongod start
+
+echo 'Installing Parse Server Dashboard and PM2';
+sleep 2;
+
+npm install -g parse-server mongodb-runner parse-dashboard pm2@latest --no-optional --no-shrinkwrap
+git clone https://github.com/ParsePlatform/parse-server-example.git
+cd parse-server-example
+
+echo
+echo 'Downloading Parse Server Dashboard Configrtion Files';
+sleep 2;
+
+sudo curl https://raw.githubusercontent.com/bajpangosh/Install-Parse-Server-on-Ubuntu/master/parse-dashboard-config.json > parse-dashboard-config.json
+sudo curl https://raw.githubusercontent.com/bajpangosh/Install-Parse-Server-on-Ubuntu/master/dashboard-running.json > dashboard-running.json
+npm -g install
+echo
+echo 'Saving PM2 for Service Live';
+sleep 2;
+echo 'Dont 4get to change Defult credntiols';
+echo
+pm2 start index.js && pm2 startup
+pm2 start dashboard-running.json && pm2 startup
+pm2 status
