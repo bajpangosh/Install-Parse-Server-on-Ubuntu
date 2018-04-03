de7d28ddca6779cc9e1ec8fa788ce45e920f23d4
Nowfal <nowfalsalahudeen@gmail.com>
2018-04-03 10:13:54 +0530

    update mongodb version

--- easy-install.sh
+++ easy-install.sh
@@ -16,7 +16,7 @@ echo 'installing Mongo DB';
 sleep 2;
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
 apt-get install -y mongodb-org
 service mongod start
