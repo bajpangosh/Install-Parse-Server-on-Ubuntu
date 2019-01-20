bfb0ff71dfc4f71f0aff13caa81aeec2628d8ed3
Bajpan Gosh <bajpangosh@gmail.com>
2019-01-20 17:46:06 +0530

    update

--- ubuntu-18.04/pd-u18.04-install.sh
+++ ubuntu-18.04/pd-u18.04-install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# GET ALL USER INPUT
+echo "Domain Name (eg. example.com)?"
+read DOMAIN
+echo "App name (eg. kloudboy)?"
+read APP_NAME
 echo 'Wellcome to Parse Server and Dashboard on Ubuntu install script';
 sleep 2;
 cd ~
@@ -17,8 +23,8 @@ service mongod start
 echo 'Installing Parse Server Dashboard and PM2';
 sleep 2;
 
-git clone https://github.com/ParsePlatform/parse-server-example.git
-cd parse-server-example
+git clone https://github.com/ParsePlatform/parse-server-example.git $APP_NAME
+cd $APP_NAME
 npm install -g parse-server mongodb-runner parse-dashboard pm2@latest --no-optional --no-shrinkwrap
 npm install express --save
 npm install parse-server --save
@@ -27,19 +33,19 @@ echo
 echo 'Downloading Parse Server Dashboard Configrtion Files';
 sleep 2;
 
-sudo curl https://raw.githubusercontent.com/bajpangosh/Install-Parse-Server-on-Ubuntu/master/parse-dashboard-config.json > parse-dashboard-config.json
-sudo curl https://raw.githubusercontent.com/bajpangosh/Install-Parse-Server-on-Ubuntu/master/dashboard-running.json > dashboard-running.json
+sudo curl https://git.kloudboy.com/parse-dashboard-config > parse-dashboard-config.json
+sudo curl https://git.kloudboy.com/dashboard-running > dashboard-running.json
 npm -g install
 echo
 echo 'Adding APP_ID and MASTER_KEY';
 sleep 2;
 APP_ID=`pwgen -s 24 1`
-sudo sed -i "s/appId: process.env.APP_ID || .*/appId: process.env.APP_ID || '$APP_ID',/" /root/parse-server-example/index.js
-sudo sed -i -e "s/APP_ID/$APP_ID/" "/root/parse-server-example/parse-dashboard-config.json"
+sudo sed -i "s/appId: process.env.APP_ID || .*/appId: process.env.APP_ID || '$APP_ID',/" /root/$APP_NAME/index.js
+sudo sed -i -e "s/APP_ID/$APP_ID/" "/root/$APP_NAME/parse-dashboard-config.json"
 
 MASTER_KEY=`pwgen -s 26 1`
-sudo sed -i "s/masterKey: process.env.MASTER_KEY || .*/masterKey: process.env.MASTER_KEY || '$MASTER_KEY',/" /root/parse-server-example/index.js
-sudo sed -i -e "s/MASTER_KEY/$MASTER_KEY/" "/root/parse-server-example/parse-dashboard-config.json"
+sudo sed -i "s/masterKey: process.env.MASTER_KEY || .*/masterKey: process.env.MASTER_KEY || '$MASTER_KEY',/" /root/$APP_NAME/index.js
+sudo sed -i -e "s/MASTER_KEY/$MASTER_KEY/" "/root/$APP_NAME/parse-dashboard-config.json"
 echo 'Happy Ending';
 echo
 pm2 start index.js && pm2 startup
