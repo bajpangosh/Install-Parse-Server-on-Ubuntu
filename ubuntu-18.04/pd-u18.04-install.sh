3556e6449dcfe16b7ddeec6bf92711d6bc80c601
Bajpan Gosh <bajpangosh@gmail.com>
2019-01-20 17:58:20 +0530

    update

--- ubuntu-18.04/pd-u18.04-install.sh
+++ ubuntu-18.04/pd-u18.04-install.sh
@@ -33,8 +33,8 @@ echo
 echo 'Downloading Parse Server Dashboard Configrtion Files';
 sleep 2;
 
-sudo curl https://git.kloudboy.com/parse-dashboard-config > parse-dashboard-config.json
-sudo curl https://git.kloudboy.com/dashboard-running > dashboard-running.json
+sudo curl https://raw.githubusercontent.com/bajpangosh/Install-Parse-Server-on-Ubuntu/master/ubuntu-18.04/parse-dashboard-config.json > parse-dashboard-config.json
+sudo curl https://raw.githubusercontent.com/bajpangosh/Install-Parse-Server-on-Ubuntu/master/ubuntu-18.04/dashboard-running.json > dashboard-running.json
 npm -g install
 echo
 echo 'Adding APP_ID and MASTER_KEY';
