c8d527f5722fc8da2cfd89ce1fa79a7f709fa153
Bajpan Gosh <bajpangosh@gmail.com>
2019-01-22 00:40:11 +0530

    update

--- ubuntu-18.04/pd-u18.04-install.sh
+++ ubuntu-18.04/pd-u18.04-install.sh
@@ -8,14 +8,16 @@ tput setaf 2; echo 'Wellcome to Parse Server and Dashboard on Ubuntu 18.04 insta
 sleep 2;
 tput sgr0
 cd ~
-echo 'installing Node Js and Nginx Server';
+tput setaf 2; echo 'installing Node Js and Nginx Server';
 sleep 2;
+tput sgr0
 apt-get update
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs pwgen nginx
 
-echo "Sit back and relax :) ......"
+tput setaf 2; echo "Sit back and relax :) ......"
 sleep 2;
+tput sgr0
 cd /etc/nginx/sites-available/
 sudo wget -O "app.$DOMAIN" https://goo.gl/2H3uGq
 sudo sed -i -e "s/app.example.com/app.$DOMAIN/" "app.$DOMAIN"
@@ -26,8 +28,9 @@ sudo sed -i -e "s/dash.example.com/dash.$DOMAIN/" "dash.$DOMAIN"
 sudo ln -s /etc/nginx/sites-available/"app.$DOMAIN" /etc/nginx/sites-enabled/
 sudo ln -s /etc/nginx/sites-available/"dash.$DOMAIN" /etc/nginx/sites-enabled/
 
-echo "Setting up Cloudflare FULL SSL"
+tput setaf 2; echo "Setting up Cloudflare FULL SSL"
 sleep 2;
+tput sgr0
 sudo mkdir /etc/nginx/ssl
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
@@ -36,30 +39,32 @@ sudo mv nginx.conf nginx.conf.backup
 sudo wget -O nginx.conf https://goo.gl/7UBeQS
 sudo systemctl reload nginx
 
-echo 'installing Mongo DB';
+tput setaf 2; echo 'installing Mongo DB';
 sleep 2;
+tput sgr0
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 apt-get update
 apt-get install -y mongodb-org
 service mongod start
 cd ~
-echo 'Installing Parse Server Dashboard and PM2';
+tput setaf 2; echo 'Installing Parse Server Dashboard and PM2';
 sleep 2;
-
+tput sgr0
 git clone https://github.com/ParsePlatform/parse-server-example.git $APP_NAME
 cd $APP_NAME
 npm install -g parse-server mongodb-runner parse-dashboard pm2@latest --no-optional --no-shrinkwrap
 echo
-echo 'Downloading Parse Server Dashboard Configrtion Files';
+tput setaf 2; echo 'Downloading Parse Server Dashboard Configrtion Files';
 sleep 2;
 
 sudo curl https://raw.githubusercontent.com/bajpangosh/Install-Parse-Server-on-Ubuntu/master/ubuntu-18.04/parse-dashboard-config.json > parse-dashboard-config.json
 sudo curl https://raw.githubusercontent.com/bajpangosh/Install-Parse-Server-on-Ubuntu/master/ubuntu-18.04/dashboard-running.json > dashboard-running.json
 npm -g install
 echo
-echo 'Adding APP_ID and MASTER_KEY';
+tput setaf 2; echo 'Adding APP_ID and MASTER_KEY';
 sleep 2;
+tput sgr0
 APP_ID=`pwgen -s 24 1`
 sudo sed -i "s/appId: process.env.APP_ID || .*/appId: process.env.APP_ID || '$APP_ID',/" /root/$APP_NAME/index.js
 sudo sed -i -e "s/APP_ID/$APP_ID/" "/root/$APP_NAME/parse-dashboard-config.json"
@@ -72,18 +77,23 @@ MASTER_KEY=`pwgen -s 26 1`
 sudo sed -i "s/masterKey: process.env.MASTER_KEY || .*/masterKey: process.env.MASTER_KEY || '$MASTER_KEY',/" /root/$APP_NAME/index.js
 sudo sed -i -e "s/MASTER_KEY/$MASTER_KEY/" "/root/$APP_NAME/parse-dashboard-config.json"
 
-PASS=`pwgen -s 7 1`
+PASS=`pwgen -s 9 1`
 sudo sed -i -e "s/PASS/$PASS/" "/root/$APP_NAME/parse-dashboard-config.json"
-echo 'Enable pm2';
+tput setaf 2; echo 'Enable pm2';
 echo
+tput sgr0
 pm2 start index.js && pm2 startup
 pm2 start dashboard-running.json && pm2 startup
 echo
 echo
 tput setaf 2; echo "Here is your Credentials"
+echo "--------------------------------"
 echo "APP_ID:   $APP_ID"
+echo
 echo "MASTER_KEY:   $MASTER_KEY"
+echo
 echo "Password:   $PASS"
+echo "--------------------------------"
 tput sgr0
 echo
 echo
